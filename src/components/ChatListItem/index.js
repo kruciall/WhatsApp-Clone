fa73a9110e336b52638ee9f0b9c21dfa1055b90f
Vinson Wong <vinson.wong93@gmail.com>
2023-03-09 14:04:28 -0500

    create ChatsScreen screen, import dummy data for chat images and text, add dayjs package and import to ChatListItem

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,7 +1,10 @@
 import { Text, View, Image, StyleSheet } from 'react-native';
-import React from 'react';
+import dayjs from 'dayjs';
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 const ChatListItem = ({ chat }) => {
+
   return (
     <View style={styles.container}>
       {/* Image  */}
@@ -13,7 +16,7 @@ const ChatListItem = ({ chat }) => {
       <View style={styles.content}>
         <View style={styles.row}>
           <Text numberOfLines={1} style={styles.name}>{chat.user.name}</Text>
-          <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
+          <Text style={styles.subTitle}>{dayjs(chat.lastMessage.createdAt).fromNow(true)}</Text>
         </View>
         <Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage.text}</Text>
       </View>
