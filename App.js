fbf41e58de022cb6ee74a77e71b6f24eefc6da3f
Vinson Wong <vinson.wong93@gmail.com>
2023-03-14 12:00:53 -0400

    implement graphql and run query for listUsers using API and graphqlOperation- contacts screen implemented dummy data from AWS

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { Amplify } from 'aws-amplify'
 import awsconfig from './src/aws-exports'
 import { withAuthenticator } from "aws-amplify-react-native";
 
-Amplify.configure(awsconfig)
+
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
  function App() {
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -57,6 +57,7 @@ const styles = StyleSheet.create({
   },
   subTitle: {
     color: 'grey',
+    
   },
 
 });
